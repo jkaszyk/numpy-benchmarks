a4688701f48f9d38ee36d91abd6e64048128684f
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2019-09-14 13:36:09 +0200

    More xtensor benchmarks

--- benchmarks/diffusion.cpp
+++ benchmarks/diffusion.cpp
@@ -14,21 +14,19 @@ using namespace xt;
 template<typename U, typename TempU>
 void diffusion(U& u, TempU& tempU, int iterNum)
 {
-    // """
-    // Apply Numpy matrix for the Forward-Euler Approximation
-    // """
-
-    float mu = 0.1;
-    for (int n = 0; n < iterNum; ++n)
-    {
-        view(tempU, range(1, -1), range(1, -1)) = view(u, range(1, -1), range(1, -1)) + mu * 
-                                                  (
-                                                        view(u, range(2, xnone()), range(1, -1)) - 2 * view(u, range(1, -1), range(1, -1)) + view(u, range(0, -2), range(1, -1)) + 
-                                                        view(u, range(1, -1), range(2, xnone())) - 2 * view(u, range(1, -1), range(1, -1)) + view(u, range(1, -1), range(0, -2))
-                                                  );
-        u = tempU;
-        tempU.fill(0.f);
-    }
+  float mu = 0.1;
+  for (int n = 0; n < iterNum; ++n) {
+    view(tempU, range(1, -1), range(1, -1)) =
+        view(u, range(1, -1), range(1, -1)) +
+        mu * (view(u, range(2, xnone()), range(1, -1)) -
+              2 * view(u, range(1, -1), range(1, -1)) +
+              view(u, range(0, -2), range(1, -1)) +
+              view(u, range(1, -1), range(2, xnone())) -
+              2 * view(u, range(1, -1), range(1, -1)) +
+              view(u, range(1, -1), range(0, -2)));
+    u = tempU;
+    tempU.fill(0.f);
+  }
 }
 
 void py_diffusion(pytensor<float, 2>& u, pytensor<float, 2>& tempU, int iterNum)
