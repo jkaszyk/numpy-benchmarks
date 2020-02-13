012f9ac0bc90737b3e4c6ff7ce32283c4f224b87
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2020-02-13 22:01:13 +0100

    Modernize log_likelihood.cpp xtensor implementation

--- numpy_benchmarks/benchmarks/log_likelihood.cpp
+++ numpy_benchmarks/benchmarks/log_likelihood.cpp
@@ -17,11 +17,7 @@ auto log_likelihood(Data const& data, double mean, double sigma)
 {
   auto s = pow((data - mean), 2) / (2 * (sigma * sigma));
   xtensor<double, 1> pdfs = exp(-s);
-#if 0
   pdfs /= std::sqrt(2 * numeric_constants<double>::PI) * sigma;
-#else
-  pdfs = pdfs / std::sqrt(2 * numeric_constants<double>::PI) * sigma;
-#endif
   pdfs = log(pdfs);
   return sum(pdfs, xt::evaluation_strategy::immediate)();
 }
