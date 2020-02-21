4bca3271081813f42afadebdd3191ee868c8da03
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2020-02-21 10:59:07 +0100

    Support periodic_dist using xtensor API

--- numpy_benchmarks/benchmarks/periodic_dist.cpp
+++ numpy_benchmarks/benchmarks/periodic_dist.cpp
@@ -4,6 +4,9 @@
 #include "xtensor/xrandom.hpp"
 #include "xtensor/xview.hpp"
 #include "xtensor/xfixed.hpp"
+#include "xtensor/xpad.hpp"
+#include "xtensor/xindex_view.hpp"
+
 #define FORCE_IMPORT_ARRAY
 
 #include "xtensor-python/pyarray.hpp"
@@ -16,13 +19,19 @@ template<typename X, typename Y, typename Z>
 auto periodic_dist(X const& x, Y const& y, Z const& z, long L, bool periodicX, bool periodicY, bool periodicZ)
 {
   auto N = x.size();
-  // see https://github.com/xtensor-stack/xtensor/issues/1557
-  auto xtemp = tile(x, {N, 1});
-  auto dx = xtemp - transpose(xtemp);
-  auto ytemp = tile(y, {N, 1});
-  auto dy = ytemp - transpose(ytemp);
-  auto ztemp = tile(z, {N, 1});
-  auto dz = ztemp - transpose(ztemp);
+  using shape_type = decltype(N);
+
+  xarray<double> xtemp = tile(x, N);
+  xtemp.reshape({N, N});
+  xtensor<double, 2> dx = xtemp - transpose(xtemp);
+
+  xarray<double> ytemp = tile(y, N);
+  ytemp.reshape({N, N});
+  xtensor<double, 2> dy = ytemp - transpose(ytemp);
+
+  xarray<double> ztemp = tile(z, N);
+  ztemp.reshape({N, N});
+  xtensor<double, 2> dz = ztemp - transpose(ztemp);
 
   if(periodicX) {
     filter(dx, dx>L/2) = filter(dx, dx > L/2) - L;
@@ -39,9 +48,8 @@ auto periodic_dist(X const& x, Y const& y, Z const& z, long L, bool periodicX, b
     filter(dz, dz<-L/2) = filter(dz, dz < -L/2) + L;
   }
 
-  auto d = sqrt(square(dx) + square(dy) + square(dz));
-
-  filter(d, d == 0) = -1;
+  xtensor<double, 2> d = sqrt(square(dx) + square(dy) + square(dz));
+  filter(d, equal(d, 0)) = -1;
 
   return std::make_tuple(d, dx, dy, dz);
 }
@@ -52,9 +60,9 @@ py_periodic_dist(pytensor<double, 1> const& x, pytensor<double, 1> const& y, pyt
   return periodic_dist(x, y, z, L, periodicX, periodicY, periodicZ);
 }
 
-PYBIND11_MODULE(xtensor_log_likelihood, m)
+PYBIND11_MODULE(xtensor_periodic_dist, m)
 {
     import_numpy();
-    m.def("log_likelihood", py_log_likelihood);
+    m.def("periodic_dist", py_periodic_dist);
 }
 
