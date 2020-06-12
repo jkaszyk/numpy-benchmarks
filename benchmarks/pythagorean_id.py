360d76c31d0dc8a933eec0e8fe18ea2bc8175c2e
Magnus Morton <magnus.morton@ed.ac.uk>
2020-06-12 13:41:47 +0100

    reordered for framework

--- benchmarks/pythagorean_id.py
+++ benchmarks/pythagorean_id.py
@@ -1,29 +1,24 @@
-import sys
-import importlib
-import timeit
+# import sys
+# import importlib
+# import timeit
 
-pkg = sys.argv[1]
-np = importlib.import_module(pkg)
+# pkg = sys.argv[1]
+# np = importlib.import_module(pkg)
 
 
 LAPTOP_MAX = 83361790
 
 SPA_MAX = 153000000
 
-size = LAPTOP_MAX
-data = np.random.random((size,))
+size = LAPTOP_MAX 
 
-def func():
-    res = np.sin(data) ** 2 + np.cos(data) ** 2
-    if np.__name__ == "delayarray":
-        res.get()
-        np.cuda.Device().synchronize()
-    elif np.__name__ == "cupy":
-        res.get()
-        np.cuda.Device().synchronize()
+def data(np):
+    return np.random.random((size,))
+
+def func(np, data):
+    return np.sin(data) ** 2 + np.cos(data) ** 2
 
 
+# print(min(timeit.repeat(func, repeat=100, number=1)))
 
-print(min(timeit.repeat(func, repeat=100, number=1)))
-
-#rint(f"time: {now-then}")
+# #rint(f"time: {now-then}")
